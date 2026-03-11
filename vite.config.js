d738746c1e29f5ba5a56043b228b5841561bb86a
Andy Krueger <165937122+andonks@users.noreply.github.com>
2026-03-11 17:05:55 -0500

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/DoDonkPachi/'
-});
+  base: '/DoDonkPachi/',
+  build: {
+    rollupOptions: {
+      input: 'src/main.jsx'
+    }
+  }
+})
